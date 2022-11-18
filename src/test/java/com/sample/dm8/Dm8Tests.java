cf218b2f576d1aff38e2084c54f8826fd7e7bab8
qlhan <qlhan@incarcloud.com>
2022-11-18 14:24:15 +0800

    feat: init
    support junit

---
+++ src/test/java/com/sample/dm8/Dm8Tests.java
@@ -0,0 +1,18 @@
+package com.sample.dm8;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Dm8Tests
+ *
+ * @author Aaric, created on 2022-11-18T14:12.
+ * @version 0.1.0-SNAPSHOT
+ */
+public class Dm8Tests {
+
+    @Test
+    public void testIsEmpty() {
+        Assertions.assertTrue(!"dm8-achieve".isEmpty());
+    }
+}
