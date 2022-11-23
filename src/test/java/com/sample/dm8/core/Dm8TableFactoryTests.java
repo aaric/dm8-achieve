bfc908e9289b908379cd89291a216f3115aac7d3
qlhan <qlhan@incarcloud.com>
2022-11-23 15:02:55 +0800

    pref: dynamic table
    Dm8TableFactoryTests.java

--- src/test/java/com/sample/dm8/core/Dm8TableFactoryTests.java
+++ src/test/java/com/sample/dm8/core/Dm8TableFactoryTests.java
@@ -1,6 +1,7 @@
 package com.sample.dm8.core;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +18,7 @@ import java.util.List;
  * @author Aaric, created on 2022-11-22T10:30.
  * @version 0.3.0-SNAPSHOT
  */
-//@Disabled
+@Disabled
 @Slf4j
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -46,10 +47,5 @@ public class Dm8TableFactoryTests {
         Dm8TableFactory factory = new Dm8TableFactory(dataSource);
         factory.dropTable(table.getName());
         factory.createTable(table);
-    }
-
-    @Test
-    public void testInsert() throws Exception {
-
     }
 }
